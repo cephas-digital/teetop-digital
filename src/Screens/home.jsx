67d91237f6f5f50be33dd5f2d578d921362f5937
Adaralegbe Ifeoluwa <ifeoluwabk4ever@gmail.com>
2023-01-24 01:13:18 +0100

    New update

--- src/Screens/home.jsx
+++ src/Screens/home.jsx
@@ -321,7 +321,7 @@ const CustomerHeader = () => {
 					<p className="text-center w-50 w50 fontReduce">
 						Ever since i started business with Teetop digital it has been all
 						smiles...they have the best customer service team ever...prompt and
-						fast response to issues. Kemtech enterprises thanks for always being
+						fast response to issues. Teetop Digital thanks for always being
 						true.
 					</p>
 				</div>
